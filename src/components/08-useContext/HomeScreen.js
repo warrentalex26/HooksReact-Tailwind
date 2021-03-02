5364192587b01d67f2559653d97e8d8f93e9db8b
warrentalex26 <warrentalex26ies@gmail.com>
2021-03-01 19:50:10 -0600

    createContext y useContext

--- src/components/08-useContext/HomeScreen.js
+++ src/components/08-useContext/HomeScreen.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+
+    const userContext = useContext(UserContext)
+    console.log(userContext);
+
     return (
         <div className='container mx-auto pt-4'>
             <h1 className='text-center mx-auto my-auto'>Home Screen</h1>
--- src/components/08-useContext/MainApp.js
+++ src/components/08-useContext/MainApp.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
+
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Alexander',
+        email: 'warrentalex26@hotmail.com'
+    }
+
     return (
         <div className='container mx-auto pt-4'>
-
-            <AppRouter/>
+            <UserContext.Provider value={user}>
+                <AppRouter />
+            </UserContext.Provider>
         </div>
     )
 }
